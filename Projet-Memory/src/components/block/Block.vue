21f8d5996a1712f4850699169e3dc1842b1da971
RathGate <mariannecorbel@hotmail.fr>
2025-04-01 20:34:07 +0200

    rebase main from mzri-ui

---
+++ Projet-Memory/src/components/block/Block.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="block">
+  <slot></slot>
+</div>
+</template>
+
+<style scoped>
+.block {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  padding: 16px 24px;
+  border-radius: 16px;
+}
+</style>
